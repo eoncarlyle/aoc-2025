a38b014e9c11d4d14b5bbb5928abac3096b626b4
Iain Schmitt <me@iainschmitt.com>
2025-12-01 09:43:17 -0600

    Still some part 2 issues even on test, will come back to

--- Program.fs
+++ Program.fs
@@ -1,4 +1,5 @@
 open day1
 open System
 
-Console.WriteLine day1.part0
+Console.WriteLine day1.part1
+Console.WriteLine day1.part2
--- day1/day1.fs
+++ day1/day1.fs
@@ -2,7 +2,7 @@ module day1
 
 open System.IO
 open System.Text.RegularExpressions
-
+open System
 
 let lines = File.ReadAllLines "./day1/test.txt"
 
@@ -41,21 +41,65 @@ let parse (row: string) =
 [<Struct>]
 type State = { Position: int; ZeroCount: int }
 
-let evaluate (state: State) (adjustment: Vector) : State =
+let evaluatePart1 (state: State) (adjustment: Vector) : State =
     let sign = if adjustment.Direction = Direction.Left then -1 else 1
     let rawPosition = state.Position + sign * adjustment.Size
 
     let position =
-        if rawPosition > 100 then rawPosition % 100
-        elif rawPosition = 100 then 0
-        elif rawPosition < 0 then 100 + (rawPosition % 100)
-        else rawPosition
+        if rawPosition > 100 then
+            rawPosition % 100
+        elif rawPosition = 100 then
+            0
+        elif rawPosition < 0 then
+            let inner = 100 + (rawPosition % 100)
+            if inner = 100 then 0 else inner
+        else
+            rawPosition
 
     let zeroIncrement = if position = 0 then 1 else 0
 
     { Position = position
       ZeroCount = state.ZeroCount + zeroIncrement }
 
+let evaluatePart2 (state: State) (adjustment: Vector) : State =
+    let sign = if adjustment.Direction = Direction.Left then -1 else 1
+    let rawPosition = state.Position + sign * adjustment.Size
 
-let part0 =
-    Array.fold evaluate { Position = 50; ZeroCount = 0 } <| Array.map parse lines
+    let position =
+        if rawPosition > 100 then
+            rawPosition % 100
+        elif rawPosition = 100 then
+            0
+        elif rawPosition < 0 then
+            let inner = 100 + (rawPosition % 100)
+            if inner = 100 then 0 else inner
+        else
+            rawPosition
+
+    let intermediateZero =
+        if (0 <= rawPosition && rawPosition <= 99) then
+            0
+        elif
+            ((rawPosition < 0 && rawPosition > -100)
+             || (rawPosition > 100 && rawPosition < 200))
+        then
+            if (rawPosition % 100 = 0) then 0 else 1
+        elif (0 <> abs (rawPosition) % 100) then
+            (abs rawPosition / 100)
+        else
+            (abs rawPosition / 100) - 1
+
+    Console.WriteLine $"{state} -> {adjustment}"
+    Console.WriteLine intermediateZero
+    let zeroIncrement = intermediateZero + (if position = 0 then 1 else 0)
+
+    { Position = position
+      ZeroCount = state.ZeroCount + zeroIncrement }
+
+let part1 =
+    Array.fold evaluatePart1 { Position = 50; ZeroCount = 0 }
+    <| Array.map parse lines
+
+let part2 =
+    Array.fold evaluatePart2 { Position = 50; ZeroCount = 0 }
+    <| Array.map parse lines
